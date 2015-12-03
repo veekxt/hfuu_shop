756586b7391afe2ae35803cf004c9419c159a142
XieTao <veekxt@gmail.com>
2015-12-04 00:24:36 +0800

    添加sql，user表设计

---
+++ sql/user.sql
@@ -0,0 +1,14 @@
+/*
+* 用户信息表
+* 暂定
+*/
+USE hfuu_shop;
+DROP TABLE IF EXISTS `user`;
+CREATE TABLE `user` (
+  `id` int(32) NOT NULL,
+  `email` char(255) COLLATE utf8_bin NOT NULL,
+  `pwd` char(255) COLLATE utf8_bin NOT NULL,
+  `name` char(255) COLLATE utf8_bin DEFAULT NULL,
+  `stu_num` char(255) COLLATE utf8_bin DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
