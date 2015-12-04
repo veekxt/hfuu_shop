830f975127d3aa47b0c6ee687d9b9cc550e8daa3
LeiLei <1164447465@qq.com>
2015-12-04 19:18:16 +0800

    add login status

--- src/src/servlet/LoginServlet.java
+++ src/src/servlet/LoginServlet.java
@@ -4,12 +4,14 @@ import java.io.IOException;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
 import src.factory.DAOFactory;
+import src.tools.GetCookie;
 import src.tools.MD5;
 import src.vo.User;
 
@@ -31,23 +33,39 @@ public class LoginServlet extends HttpServlet {
 	
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		String inputEmail=request.getParameter("inputEmail");
-	
-		String inputPassword=request.getParameter("inputPassword");
-	
+	    String inputPassword=request.getParameter("inputPassword");
+//	    Cookie[]cookies=request.getCookies();
+//	   String cookieValue= GetCookie.getCookie(cookies, "idNum");
+//	   if(cookieValue!=null){
+//			HttpSession session=request.getSession();
+//		     String sessionId=session.getId();
+//		     session.setAttribute("Email",cookieValue);
+//			session.setAttribute("isLogin", true);
+//		 request.getRequestDispatcher("/index.jsp").forward(request, response);  
+//		   
+//	   }
+	    
 			try {
 				if(DAOFactory.getIUserDAOInstance().findByEmail(inputEmail)!=null){
 				
 				User user=	DAOFactory.getIUserDAOInstance().findByEmail(inputEmail);
                 String pass=	MD5.getMD5(MD5.getMD5(inputPassword));
 					if(user.getPwd().equals(pass)){
-						//session状态
-//					HttpSession session=request.getSession();
-//				     String sessionId=	session.getId();
-//				     session.setAttribute("sessionId",sessionId);
-//					session.setAttribute("loginstates", true);
+				   String checkstate="sss";
+				  checkstate =(String)request.getParameter("checkbox");
+				  
+				   if(checkstate!=null&&checkstate.equals("on")){
+					   Cookie cookie=new Cookie("idNum",user.getEmail());
+					   cookie.setMaxAge(60*60);
+					   response.addCookie(cookie);
+					   }
+					HttpSession session=request.getSession();
+				     String sessionId=session.getId();
+				     session.setAttribute("Email",user.getEmail());
+					session.setAttribute("isLogin", true);
 						request.getRequestDispatcher("/index.jsp").forward(request, response);
 					}else{
-						
+						request.setAttribute("isLogin", false);
 						request.getRequestDispatcher("/user/login.jsp").forward(request, response);
 					}
 					
---
+++ src/src/tools/GetCookie.java
@@ -0,0 +1,26 @@
+package src.tools;
+
+import javax.servlet.http.Cookie;
+
+public class GetCookie {
+	public static String getCookie(Cookie[] cookies,String cookiename){
+		for(Cookie cookie:cookies){
+			if(cookie.getName().equals(cookiename)){
+				  
+				
+				String  cookieValue=cookie.getValue();
+				
+				return cookieValue;
+				
+			}
+			
+			
+		}
+		
+return null;
+		
+		
+		
+	}
+
+}
