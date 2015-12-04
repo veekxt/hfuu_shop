2496ecd5a3aae9e35735acd007134353e0f565d7
XieTao <veekxt@gmail.com>
2015-12-04 10:57:48 +0800

    商品表设计

--- sql/user.sql
+++ sql/user.sql
@@ -4,11 +4,27 @@
 */
 USE hfuu_shop;
 DROP TABLE IF EXISTS `user`;
-CREATE TABLE `user` (
+CREATE TA BLE `user` (
   `id` int(32) NOT NULL,
   `email` char(255) COLLATE utf8_bin NOT NULL,
   `pwd` char(255) COLLATE utf8_bin NOT NULL,
   `name` char(255) COLLATE utf8_bin DEFAULT NULL,
   `stu_num` char(255) COLLATE utf8_bin DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+/*
+* 商品信息表
+* 暂定
+*/
+DROP TABLE IF EXISTS `goods`;
+CREATE TABLE `goods` (
+  `id` int(11) NOT NULL,
+  `image` char(255) COLLATE utf8_bin NOT NULL,
+  `type_id` int(11) NOT NULL COMMENT '类型id',
+  `name` char(255) COLLATE utf8_bin NOT NULL COMMENT '商品名',
+  `num` int(11) NOT NULL COMMENT '数量',
+  `price` float NOT NULL,
+  `content` varchar(255) COLLATE utf8_bin NOT NULL,
+  `producter_id` int(11) NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
