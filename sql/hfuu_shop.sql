a3ca40e77799bd733b4f99a0292a5c55f1ef8e43
XieTao <veekxt@gmail.com>
2015-12-23 18:56:56 +0800

    goods增加

--- sql/hfuu_shop.sql
+++ sql/hfuu_shop.sql
@@ -10,7 +10,7 @@ Target Server Type    : MYSQL
 Target Server Version : 50621
 File Encoding         : 65001
 
-Date: 2015-12-23 12:36:18
+Date: 2015-12-23 18:51:19
 */
 
 SET FOREIGN_KEY_CHECKS=0;
@@ -36,12 +36,17 @@ CREATE TABLE `goods` (
 -- ----------------------------
 -- Records of goods
 -- ----------------------------
-INSERT INTO `goods` VALUES ('1', 'static/goods_img/1.jpg', '4', '笔记本', '1', '4000', '2', '二手笔记本，8成新，I7处理器', '1017', '2015-12-12 12:10:10');
+INSERT INTO `goods` VALUES ('1', 'static/goods_img/1.jpg', '4', '笔记本', '1', '4000', '2', '二手笔记本，8成新，I7处理器', '10', '2015-12-12 12:10:10');
 INSERT INTO `goods` VALUES ('2', 'static/goods_img/2.jpg', '2', '被套', '1', '30', '2', '二手被套', '1017', '2015-12-16 02:34:01');
 INSERT INTO `goods` VALUES ('3', 'static/goods_img/3.jpg', '2', '自行车', '1', '50', '2', '二手自行车', '1017', '2015-12-11 11:22:33');
-INSERT INTO `goods` VALUES ('4', 'static/goods_img/4.jpg', '5', '网球拍', '1', '50', '2', '二手网球拍，用过几天九成新<呵呵>', '1018', '2015-12-17 11:00:16');
-INSERT INTO `goods` VALUES ('5', 'static/goods_img/5.jpg', '5', '篮球', '1', '80', '2', '全牛皮篮球，', '1018', '2015-12-17 11:02:57');
-INSERT INTO `goods` VALUES ('6', 'static/goods_img/6.jpg', '2', '懒人桌', '1', '15', '2', '加固型懒人桌，九成新', '1018', '2015-12-17 11:05:00');
+INSERT INTO `goods` VALUES ('4', 'static/goods_img/4.jpg', '5', '网球拍', '1', '50', '2', '二手网球拍，用过几天，九成新', '123', '2015-12-17 11:00:16');
+INSERT INTO `goods` VALUES ('5', 'static/goods_img/5.jpg', '5', '篮球', '1', '80', '2', '全牛皮篮球，', '9', '2015-12-17 11:02:57');
+INSERT INTO `goods` VALUES ('6', 'static/goods_img/6.jpg', '2', '懒人桌', '1', '15', '2', '加固型懒人桌，九成新', '123', '2015-12-17 11:05:00');
+INSERT INTO `goods` VALUES ('7', 'static/goods_img/7.jpg', '2', '考研书', '1', '30', '2', '聚星文登考研', '123', '2015-12-21 11:07:22');
+INSERT INTO `goods` VALUES ('8', 'static/goods_img/8.jpg', '2', '公务员书', '1', '30', '2', '公务员考试书籍，9成新', '123', '2015-12-23 16:02:21');
+INSERT INTO `goods` VALUES ('9', 'static/goods_img/9.jpg', '3', '凉席', '1', '60', '2', '寝室牛皮凉席', '123', '2015-12-23 16:20:46');
+INSERT INTO `goods` VALUES ('10', 'static/goods_img/10.jpg', '3', '纯棉枕头', '1', '50', '2', '纯棉枕头', '123', '2015-12-23 16:21:37');
+INSERT INTO `goods` VALUES ('11', 'static/goods_img/11.jpg', '3', 'LED台灯护眼学习卧室床头书桌大学生寝室插电节能USB调光夹子台灯', '1', '100', '2', '灯光颜色默认自然光，轻微偏黄的灯光颜色，台灯默认USB接口，台灯供电方式：1，可用一切手机充电器直接插220V家用插座。 2，可接电脑USB。 3，可接手机充电宝供电。（注：这款不是充电台灯，不带蓄电池，必须连着电源使用）', '123', '2015-12-23 16:29:32');
 
 -- ----------------------------
 -- Table structure for `message`
@@ -55,7 +60,7 @@ CREATE TABLE `message` (
   `mess_id` int(11) unsigned NOT NULL AUTO_INCREMENT,
   `mess_type` int(11) DEFAULT NULL,
   PRIMARY KEY (`mess_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+) ENGINE=InnoDB AUTO_INCREMENT=23 DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
 -- ----------------------------
 -- Records of message
@@ -115,9 +120,10 @@ CREATE TABLE `user` (
 INSERT INTO `user` VALUES (null, '1', 'hfuu_shop@163.com', '8560ef54213c8b32e89a24ccc323a79e', '交易系统', null, null, null, '0');
 INSERT INTO `user` VALUES (null, '9', 'dandan@qq.com', '14e1b600b1fd579f47433b88e8d85291', '朱雷雷', null, null, null, '0');
 INSERT INTO `user` VALUES ('static/user_img/10', '10', 'veekxt@gmail.com', '14e1b600b1fd579f47433b88e8d85291', '解涛', null, null, '15256925578', '0');
+INSERT INTO `user` VALUES (null, '123', '1300573251@qq.com', '191016dc3346309bee3403f55f77e871', '张剑', null, null, null, '0');
 INSERT INTO `user` VALUES (null, '1017', '1050026@qq.com', 'acd09f1f204179b957001f53f411899b', '陈生辉', null, null, '13245634567', '0');
-INSERT INTO `user` VALUES (null, '1018', '1300573251@qq.com', '191016dc3346309bee3403f55f77e871', '张剑', null, null, null, '0');
 INSERT INTO `user` VALUES (null, '1019', '18256989168@163.com', '191016dc3346309bee3403f55f77e871', '张剑', null, null, null, '0');
+
 DROP TRIGGER IF EXISTS `notify_auditing`;
 DELIMITER ;;
 CREATE TRIGGER `notify_auditing` AFTER UPDATE ON `goods` FOR EACH ROW begin
@@ -134,10 +140,10 @@ end if;
 end
 ;;
 DELIMITER ;
+
 DROP TRIGGER IF EXISTS `change_user_mess_num`;
 DELIMITER ;;
 CREATE TRIGGER `change_user_mess_num` AFTER INSERT ON `message` FOR EACH ROW update user set mess_num=mess_num+1 where id=new.mess_to_id
-;
 ;;
 DELIMITER ;
 DROP TRIGGER IF EXISTS `update_goods_status`;
--- src/src/tools/StaticVar.java
+++ src/src/tools/StaticVar.java
@@ -1,7 +1,7 @@
 package src.tools;
 
 public class StaticVar {
-	public static final int PERPAGE_MESS=10;
-	public static final int PERPAGE_GOODS=15;
+	public static int PERPAGE_MESS=10;
+	public static int PERPAGE_GOODS=15;
 
 }
