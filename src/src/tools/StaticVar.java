8068c35f96ff5df9be5c87f1457dcb5e35d8b688
XieTao <veekxt@gmail.com>
2015-12-23 22:46:45 +0800

    分页调整到正常值

--- src/src/tools/StaticVar.java
+++ src/src/tools/StaticVar.java
@@ -3,7 +3,7 @@ package src.tools;
 public class StaticVar {
 	public static int PERPAGE_MESS=10;
 	public static int PERPAGE_GOODS=15;
-	public static int PERPAGE_COLLECT=1;
+	public static int PERPAGE_COLLECT=7;
 	
 
 }
