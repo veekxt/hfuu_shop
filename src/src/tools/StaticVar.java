0b08c83180d1845ba7601d70194726a0f5614776
XieTao <veekxt@gmail.com>
2015-12-29 19:46:47 +0800

    更新注释

--- src/src/tools/StaticVar.java
+++ src/src/tools/StaticVar.java
@@ -3,6 +3,6 @@ package src.tools;
 public class StaticVar {
 	//这里放一些网站配置数据
 	public static int PERPAGE_MESS=10;//消息页每页显示个数
-	public static int PERPAGE_GOODS=11;//...
-	public static int PERPAGE_COLLECT=7;//...
+	public static int PERPAGE_GOODS=11;//物品浏览页每页显示个数
+	public static int PERPAGE_COLLECT=7;//收藏页每页显示个数
 }
